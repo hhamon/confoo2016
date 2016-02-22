978216a41b8475e137e0434d6802a69ccbfc4016
Hugo Hamon <hugo.hamon@sensiolabs.com>
2016-02-22 15:42:19 -0500

    Release of new version 0.0.2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,7 +5,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    const APP_VERSION = '0.0.1';
+    const APP_VERSION = '0.0.2';
 
     public function registerBundles()
     {
